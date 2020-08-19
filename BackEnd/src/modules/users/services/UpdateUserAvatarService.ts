e923a4eef0277088d92b2b779369b73c42472486
MailsonSilva <mailsonfcsilva@gmail.com>
2020-08-18 22:11:16 -0300

    Erro::putObject

--- BackEnd/src/modules/users/services/UpdateUserAvatarService.ts
+++ BackEnd/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,8 +1,5 @@
 import {inject, injectable} from 'tsyringe';
-import path from 'path';
-import fs from 'fs';
 import User from '../infra/typeorm/entities/User';
-import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
